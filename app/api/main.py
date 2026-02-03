99b4d57829eb9f781ec21583743ddbd8d077aa79
PsychCodez <itsmedrrg@gmail.com>
2026-02-03 09:45:25 +0530

    CORS added

--- app/api/main.py
+++ app/api/main.py
@@ -1,8 +1,15 @@
 from fastapi import FastAPI
 from typing import List
 from pydantic import BaseModel
-
+from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 polls = {}  # in-memory; later you can add SQLite
 
